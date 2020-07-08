296b624df1b2de2b35aa44136442bd7725020038
TimZhang <binbinverygood@qq.com>
2020-07-08 11:20:32 +0800

    Update fbutton.dart

--- lib/fbutton.dart
+++ lib/fbutton.dart
@@ -423,8 +423,8 @@ class _FButton extends State<FButton> {
 
     /// side
     Color sideColor = enabled
-        ? shadowColor ?? Colors.transparent
-        : shadowColor == null
+        ? widget.shadowColor ?? Colors.transparent
+        : widget.shadowColor == null
             ? Colors.transparent
             : widget.strokeColor.withOpacity(disableOpacity);
     BorderSide borderSide = BorderSide(
